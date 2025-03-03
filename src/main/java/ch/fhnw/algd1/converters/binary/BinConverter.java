b16dde37a635e5ab5c66266dee46f6ddf4e586a0
urielBrawer <uriel.brawer@students.fhnw.ch>
2025-03-03 10:33:15 +0100

    solution for toString method: with algorithm impl or library or bitshift

--- src/main/java/ch/fhnw/algd1/converters/binary/BinConverter.java
+++ src/main/java/ch/fhnw/algd1/converters/binary/BinConverter.java
@@ -1,10 +1,52 @@
 package ch.fhnw.algd1.converters.binary;
 
+import java.util.ArrayList;
+
 public class BinConverter {
 	public static String toString(int x) {
 		// TODO expect x to be in range [-128, 127], return string with 8 binary
+
+		String option = "bitshift";
+		String binary = "00000000";
+		switch (option) {
+			case "diy":
+				// diy solution of algorithm:
+				String result = "";
+				while (x != 0) {
+					System.out.println("loop with x = " + x);
+					if (x % 2 == 0) {
+						result = result + "0";
+						System.out.println(result);
+					} else {
+						result = result + "1";
+						x = x - 1;
+					}
+					x = x / 2;
+				}
+				System.out.println(result);
+				binary = String.format("%8s", result).replace(' ', '0');
+				System.out.println("diy: " + binary);
+				break;
+			case "Library":
+				// built in solution:
+				binary = String.format("%8s", Integer.toBinaryString(x)).replace(' ', '0');
+				System.out.println("Library: " + binary);
+				break;
+			case "bitshift":
+				StringBuilder res = new StringBuilder();
+				for (int i = 0; i < 8; i++) {
+					int bit = (x >> i) & 1;
+					res.append(bit);
+				}
+				res.reverse();
+				System.out.println(res);
+				binary = res.toString();
+				break;
+		}
+
 		// digits representing x in 2-complement
-		return "00000000";
+		return binary;
+
 	}
 
 	public static int fromString(String text) {
